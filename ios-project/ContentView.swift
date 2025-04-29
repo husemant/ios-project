767385d3de48cc3cdaa4b341ebb5d6629f6fd678
Tanner Huseman <husemant@merrimack.edu>
2025-04-29 19:17:36 -0400

    Initial Commit

---
+++ ios-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-project
+//
+//  Created by Tanner Huseman on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-project/ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_projectApp.swift
+//  ios-project
+//
+//  Created by Tanner Huseman on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
